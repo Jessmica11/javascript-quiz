b8d5bae4744dd3922da9677ab6452eb436138b8e
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2023-10-16 12:15:48 -0400

    added js script for hiding, showing, and running through questions object

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,14 @@
 var startButton = document.querySelector("#start-button");
 var timerEl = document.querySelector("#seconds-left");
-var userChoice = document.querySelector(".option-button");
+var userChoice = document.querySelector(".btn");
 var userScore = 0;
 
   // add event listener to the button
 startButton.addEventListener('click', setTime);
 
 var timeEl = document.querySelector("#seconds-left");
-startButton.addEventListener('click', changeContent);
+startButton.addEventListener("click", changeContent);
+startButton.addEventListener("click", showQuiz);
 
 var mainEl = document.getElementById("timer");
 
@@ -28,46 +29,83 @@ function setTime() {
   }, 1000);
 }
 
-// Function to remove start button and replace with other text
+  // Function to remove start button and replace with other text (to prevent user from clicking over and over)
   function changeContent() {
     startButton.innerHTML = "Answer the questions below";
   }
 
-  // Q & A object with all possibilities
+  // I want the quiz card to show when the user clicks the start button
+  function showQuiz() {
+    var quizCard = document.querySelector(".quiz");
+    if (quizCard.style.display === "none") {
+      quizCard.style.display = "block";
+    } else{
+      quizCard.style.display = "none";
+  }
+}
 
-function startQuiz () {
-  
-  var questionArea = document.querySelector(".question-area");
-  var answerArea = document.querySelector("#answer-area");
+  // Q & A object with all possibilities
+  var questionArea = document.getElementById("question");
+  var answerArea = document.getElementById("answer-buttons");
   var current = 0;
   
-     // An object that holds all the questions + possible answers.
-     // In the array --> also provide correct answer
-      var quizQuestions = [
-        { prompt: "Which answer is a Javascript data type?", 
-          answers: [
-            {text: "Loop", confirm: false},
-            {text: "Boolean", confirm: true},
-            {text: "Property", confirm: false},
-            {text: "Variable", confirm: false},]
-        },
+  // An object that holds all the questions + possible answers.
+  // In the array --> also provide correct answer
+  const questions = [
+     { question: "Which answer is a Javascript data type?", 
+       choices: ["Loop", "Boolean", "Property", "Variable"],
+       answer: 1
+     },
 
-        { prompt: "What is javascript used for?", 
-          answers: [
-            {text: "Styling", confirm: false},
-            {text: "Structure", confirm: false},
-            {text: "User Interactivity", confirm: true},
-            {text: "Showing off", confirm: false},]
-        },
+    { question: "What is javascript used for?", 
+      choices: ["Styling", "Structure", "User Interactivity", "Showing off"],
+      answer: 2
+    },
       
-        { prompt: "Do we still use Javascript on modern pages today?", 
-          answers: [
-            {text: "Yes!", confirm: true},
-            {text: "Nah, not really", confirm: false},]
-        },
-      ]
-      };
+    { question: "What does DOM stand for?", 
+      choices: ["Dog Operations Manager", "Document Object Model", "Do Ostriches Math?", "Don't Organize MyStuff"],
+      answer: 1
+    },
+  ];
 
+// need a function to actually get window ready for user's answers; start at zero for grade, etc
+let currentQuestion = 0;
+let correctAnswers = 0;
 
-     
+// show the questions from the object in the right place in the quiz <div>
+function showQuestion() {
+  const questionText = document.getElementById("question");
+  questionText.textContent = questions[currentQuestion].question;
 
+  const choices = document.querySelectorAll(".btn");
+  choices.forEach((choice, index) => {
+    choice.textContent = questions[currentQuestion].choices[index];
+  });
+
+  const feedback = document.getElementById("feedback");
+  feedback.textContent = "";
+}
+
+function checkAnswer(selected) {
+  const feedback = document.getElementById("feedback");
+  if (selected === questions[currentQuestion].answer) {
+    feedback.textContent = "Correct!";
+    correctAnswers++;
+  } else {
+    feedback.textContent = "Incorrect!";
+    secondsLeft--;
+  }
+
+  setTimeout(() => {
+    currentQuestion++;
+
+    if (currentQuestion < questions.length) {
+      showQuestion();
+    } else {
+      const quizContainer = document.querySelector(".quiz");
+      quizContainer.innerHTML = `<p>You got ${correctAnswers} out of ${questions.length} questions.</p>`;
+    }
+  }, 2000);
+}
+
+showQuestion();
